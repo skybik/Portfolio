02d27abddaab7773e39667e12df0f84b84545233
skybik <babaevskiu@gmail.com>
2023-04-10 13:24:44 -0700

    add header, content and footer components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,17 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import "../App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>My Portfolio</h1>
-      </header>
-      <main>
-        <section>
-          <h2>About Me</h2>
-          <p>Hi, I'm Aiden. I'm a web developer based in San Diego, CA.</p>
-        </section>
-        <section>
-          <h2>My Skills</h2>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Java Script</li>
-          </ul>
-        </section>
-        <section>
-          <h2>My Work</h2>
-          <p>Currently learning React.</p>
-        </section>
-      </main>
-      <footer>
-        <p>Copyright © 2023</p>
-      </footer>
+      <div className="container">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main>
+      <section>
+        <h2>About Me</h2>
+        <p>Hi, I'm Aiden. I'm a web developer based in San Diego, CA.</p>
+      </section>
+      <section>
+        <h2>My Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Java Script</li>
+        </ul>
+      </section>
+      <section>
+        <h2>My Work</h2>
+        <p>Currently learning React.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Content;
